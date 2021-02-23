67aabcedf716197dd8424e8103715b41bd43e4df
JordanMiller6 <78764567+JordanMiller6@users.noreply.github.com>
2021-02-23 09:09:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var containerEle = document.body.querySelector(".total");
+
+var bill=Number(prompt("What is the bill?"));
+
+var tax=Number(1.07);
+
+var tip=(1.05);
+
+var all=Number(bill*tax*tip)
+var round = all.toFixed(2);
+document.body.querySelector(".total").innerHTML = "Your bill is $"+round+"!";
